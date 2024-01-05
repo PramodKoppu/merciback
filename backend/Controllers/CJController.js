81a863c4d3ea477a47f2f08b43cd26dbe875d177
Pramod <pramodkumarkoppu@gmail.com>
2024-01-05 17:16:52 +0530

    status update

--- backend/Controllers/CJController.js
+++ backend/Controllers/CJController.js
@@ -172,7 +172,7 @@ const getProducts = async (req, res) => {
     if (products.length > 0) {
       return res.status(200).json({ status: 200, productslist: products, totalPages: totalPages, totalProducts: totalProducts });
     } else {
-      return res.status(404).json({ status: 404, message: "No products found." });
+      return res.status(200).json({ status: 404, message: "No products found." });
     }
   } catch (error) {
     console.error(error);
