a4bdca57195aeb1837df4cb96085754b1d323a84
Pramod <pramodkumarkoppu@gmail.com>
2025-03-09 12:27:19 +0530

    Changes to percentages

--- backend/utils/distribution.js
+++ backend/utils/distribution.js
@@ -6,19 +6,19 @@ const distributePayment = (merci_tree, merci_level, total, monthly) => {
       Level4: 0
     };
 
-    const highComm = monthly ? 0.10 : 0.25;
+    const highComm = monthly ? 0.10 : 0.50;
 
     if (merci_level === 'Level 4') {
       distribution.Level4 = total * highComm;
-      distribution.Level3 = total * 0.05;
-      distribution.Level2 = total * 0.025;
+      distribution.Level3 = total * 0.10;
+      distribution.Level2 = total * 0.05;
       distribution.Level1 = total - (distribution.Level4 + distribution.Level3 + distribution.Level2);
     } else if (merci_level === 'Level 3') {
-      distribution.Level3 = total * 0.05;
-      distribution.Level2 = total * 0.025;
+      distribution.Level3 = total * 0.10;
+      distribution.Level2 = total * 0.05;
       distribution.Level1 = total - (distribution.Level3 + distribution.Level2);
     } else if (merci_level === 'Level 2') {
-      distribution.Level2 = total * 0.025;
+      distribution.Level2 = total * 0.05;
       distribution.Level1 = total - distribution.Level2;
     }
 
